221f1084896bab9926eb4ef9041a93b0c1e537e1
danis <danis12314@gmail.com>
2013-09-12 16:59:41 +0800

    初始状态

---
+++ Fabric_Beta1/Fabric_Beta1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fabric_Beta1
+//
+//  Created by Danis on 13-9-12.
+//  Copyright (c) 2013年 Danis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LLDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LLDAppDelegate class]));
+    }
+}
